24a9b9ecc852dc589828d986d75bc675d38b6802
X-rated-Curse <49300329+X-rated-Curse@users.noreply.github.com>
2019-11-21 22:37:51 +0900

    implemented Card, Volunteer Manager
    synchronized date format with client application.

--- server/src/CardManager.java
+++ server/src/CardManager.java
@@ -1,33 +1,27 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-
 public class CardManager {
-	private Card card;
-	Mediator mediator;
-	SimpleDateFormat dataformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	static private Card card = null;
 	
 	public CardManager(Mediator mediator) {
-		this.mediator = mediator;
-		
 	}
 	
 	//converse data String into card
-	public Card converse(String[] data)  {
-		//data set : name,pNumber,date + vMsID;
+	public Card converse(String[] data) {
+		SimpleDateFormat transFormat = new SimpleDateFormat("yyyyMMddHHmm");
 		try {
-			card = new Card(data[1], Integer.parseInt(data[2]),
-					dataformat.parse(data[3]));
-			
-		} catch (NumberFormatException | ParseException e) {
+			if(data.length == 5)
+				card = new Card(data[1], Integer.parseInt(data[2]), transFormat.parse(data[3]));
+			else
+				card = new Card(data[1], Integer.parseInt(data[2]), transFormat.parse(data[3]), data[4]);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		if(data.length ==5 && data[4] != null) {
-			card.setVMS_ID(data[4]);
-		}
-		
-		return this.card;
+		return card;
 	}
 }
